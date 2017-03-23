24815c0cb3ac3a9d08e7bee9d9ed38d264781d45
jaykilleen <me@jaykilleen.com>
2017-03-23 23:24:59 +1000

    Continue to add snippets for the Table reference. Add Seeds to test snippet and syntax highlighting for development or demonstration purposes

---
+++ spec/seeds.m
@@ -0,0 +1,23 @@
+// Comments
+
+let
+  Source = "SimpleExample"
+in
+  Source
+
+---
+
+let
+  Source = "AdvancedExample"
+  CreateEmptyTable = #table(
+    {"Column1","Column2"},
+      {
+        {1,"A"},
+        {2,"B"}
+      }
+    }
+  )
+in
+  CreateEmptyTable
+
+---
