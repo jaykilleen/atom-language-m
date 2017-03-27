eaa5bc5bd1142e18dde6e930a0d4b6bc5358ac02
jaykilleen <me@jaykilleen.com>
2017-03-27 14:25:33 +1000

    Cleaned up grammar file

--- spec/seeds.m
+++ spec/seeds.m
@@ -1,23 +1,40 @@
 // Comments
-
 let
-  Source = "SimpleExample"
-in
-  Source
-
----
-
-let
-  Source = "AdvancedExample"
-  CreateEmptyTable = #table(
-    {"Column1","Column2"},
-      {
-        {1,"A"},
-        {2,"B"}
-      }
+  Source = Excel.CurrentWorkbook(){[Name="City"]}[Content],
+  change_type = Table.TransformColumnTypes(
+    Source,
+    {
+      {"Id", Int64.Type},
+      {"Name", type text}
     }
-  )
-in
-  CreateEmptyTable
+  ),
 
----
+  add_pop_m = Table.AddColumn(
+    change_type,
+    "Population(m)",
+    (CityRow) =>
+      Table.RowCount(
+        Table.SelectRows(
+          People,
+          each
+            [CityId] = CityRow[Id]))),
+
+  add_pop_so = Table.AddColumn(
+    add_pop_m,
+    "Population(so)",
+      each
+        let Id=[Id]
+        in Table.RowCount(
+          Table.SelectRows(
+            People,
+            each
+              [CityId] = Id))),
+
+  #"add pop fm" = Table.AddColumn(
+    add_pop_so,
+    "Population(f)",
+    each
+      f_countif(People, "CityId",[Id]))
+
+in
+   addpopfm
